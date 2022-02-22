a79bf0ac7dfb639bf2944bcca25d8c901e91ac58
Allan M Alegretti <allanalegretti@gmail.com>
2022-02-22 15:47:07 -0300

    BigDecimal

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -1,11 +1,13 @@
 package com.dio;
 
+import java.math.BigDecimal;
+
 import com.dio.base.Order;
 
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		Order order = new Order ( "code1234");
+		Order order = new Order ("code1234", new BigDecimal("99.99"));
 		System.out.println(order);
 	}
 }
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -1,16 +1,23 @@
 package com.dio.base;
 
+import java.math.BigDecimal;
+
 public class Order {
 	
 	private final String code;
+	private final BigDecimal totalValue;
 	
-	public Order (String code) {
+	public Order (String code, BigDecimal totalValue) {
 		this.code = code;
+		this.totalValue = totalValue;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Order={" + "code='"+code+"'"+"}";
+		return "Order={" +
+				"code= '"+code+"'"+
+				" totalValue= "+ totalValue+
+				"}";
 	}
 
 }
