f2c728c8144a5ccbfb2faa75b1e6322ffe01baea
Allan M Alegretti <allanalegretti@gmail.com>
2022-02-22 16:04:21 -0300

    calculateFee

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -1,13 +1,11 @@
 package com.dio;
 
-import java.math.BigDecimal;
-
 import com.dio.base.Order;
 
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		Order order = new Order ("code1234", new BigDecimal("99.99"));
+		final Order order = new Order ("code", 99.99);
 		System.out.println(order);
 	}
 }
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -1,23 +1,21 @@
 package com.dio.base;
 
-import java.math.BigDecimal;
-
 public class Order {
 	
 	private final String code;
-	private final BigDecimal totalValue;
+	private final double totalValue;
 	
-	public Order (String code, BigDecimal totalValue) {
+	public Order (String code, double totalValue) {
 		this.code = code;
 		this.totalValue = totalValue;
 	}
 
-	@Override
-	public String toString() {
-		return "Order={" +
-				"code= '"+code+"'"+
-				" totalValue= "+ totalValue+
-				"}";
+	public double calculateFee () {
+		if (totalValue > 100) {
+			return totalValue * 5;
+		} else {
+			return totalValue;
+		}
 	}
 
 }
