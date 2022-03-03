131a186720b990f4f79ab3fcee6673b2e20e5404
Allan M Alegretti <allanalegretti@gmail.com>
2022-03-03 16:56:18 -0300

    Adicionado o exercicio Quadrilatero

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -8,23 +8,33 @@ public class Main {
     public static void main(String[] args) {
 
         // Calculadora
-        System.out.println("Exercício calculadora");
+        System.out.println("Exercício da calculadora:");
         Calculadora.soma(3, 6);
         Calculadora.subtracao(9, 1.8);
         Calculadora.multiplicacao(7, 8);
         Calculadora.divisao(5, 2.5);
+        System.out.println();
 
         // Mensagem
-        System.out.println("Exercício mensagem");
+        System.out.println("Exercício da mensagem:");
         Mensagem.obterMensagem(9);
         Mensagem.obterMensagem(14);
         Mensagem.obterMensagem(1);
+        System.out.println();
 
         // Empréstimo
-        System.out.println("Exercício empréstimo");
+        System.out.println("Exercício do empréstimo:");
         Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
         Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
         Emprestimo.calcular(1000, 5);
+        System.out.println();
+        
+        // Quadrilátero
+        System.out.println("Exercício do quadrilátero:");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
 
     }
 }
